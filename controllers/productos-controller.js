ade79af8246fbf9204eeabacd19413ebba269b68
keiMfc <129462940+keiMfc@users.noreply.github.com>
2023-08-03 18:08:40 -0400

    leer todos los productos en gestor de productos

--- controllers/productos-controller.js
+++ controllers/productos-controller.js
@@ -1,10 +1,9 @@
-import {clientServices} from "../services/client-services.js";
+import { clientServices } from "../services/client-services.js";
 
 const newProduct = (id, categoria, nombre, precio, imagen) => {
    const card = document.createElement("div");
    const contenido = `<div class="contenedor_card_producto" id="${id}">
-       <img src="${imagen}" alt="${nombre}"
-           class="contenedor_card_producto_imagen">
+       <img src="${imagen}" alt="${nombre}" class="contenedor_card_producto_imagen">
        <h4 class="contenedor_card_producto_descripcion">${nombre}</h4>
        <h5 class="contenedor_card_producto_precio">${precio}</h5>
        <button class="contenedor_card_producto_verProducto button-text"><a href="../screens/descripcion-producto.html?id=${id}">Ver producto</a></button>
@@ -14,31 +13,36 @@ const newProduct = (id, categoria, nombre, precio, imagen) => {
    card.classList.add("card");
    card.dataset.id = id;
 
-   return card
+   return card;
 };
 
 const productStarWars = document.querySelector("[data-star-wars]");
 const productConsolas = document.querySelector("[data-consolas]");
 const productDiversos = document.querySelector("[data-diversos]");
+const allProducts = document.querySelector("[data-all-products]");
 
-const loadProduct = async() => {
+const loadProduct = async () => {
     const data = await clientServices.listProducts();
 
     data.forEach((product) => {
-        const newCard = newProduct(product.id, product.categoria, product.nombre, product.precio, product.imagen);
+        const newCard = newProduct(
+            product.id, 
+            product.categoria, 
+            product.nombre, 
+            product.precio, 
+            product.imagen
+        );
 
-        if(product.categoria === "Star Wars") {
+        if (product.categoria === "Star Wars" && productStarWars) {
             productStarWars.appendChild(newCard);
-        }
-        if(product.categoria === "Consolas") {
+        } else if (product.categoria === "Consolas" && productConsolas) {
             productConsolas.appendChild(newCard);
-        }
-        if(product.categoria === "Diversos") {
+        } else if (product.categoria === "Diversos" && productDiversos) {
             productDiversos.appendChild(newCard);
+        } else if (allProducts) {
+            allProducts.appendChild(newCard);
         }
     });
 };
 
 loadProduct();
-
-
